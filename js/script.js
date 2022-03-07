a6b3e480d9f38e0623c5ed9b38b17fd06f3c0de2
Valencia <starrv2011@gmail.com>
2022-03-07 10:55:48 -0500

    updated projects page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
 	$(".project").click(function()
 	{
 		var link=$(this).find("a").attr("href");
 		window.open(link,"_blank");
 	});
-});
+});*/
